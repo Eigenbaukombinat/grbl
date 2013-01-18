3ea482ae7b8fdf6d530dc5cd9364bc6f813d8b07
Elijah Insua <tmpvar@gmail.com>
2013-01-18 09:31:13 -0700

    Enable the use of RC airplane motor as spindle (PPM)

--- spindle_control.c
+++ spindle_control.c
@@ -23,35 +23,70 @@
 #include "spindle_control.h"
 #include "planner.h"
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
 static uint8_t current_direction;
+
+#define MAX 65535
 
 void spindle_init()
 {
   current_direction = 0;
   SPINDLE_ENABLE_DDR |= (1<<SPINDLE_ENABLE_BIT);
-  SPINDLE_DIRECTION_DDR |= (1<<SPINDLE_DIRECTION_BIT);  
+  SPINDLE_DIRECTION_DDR |= (1<<SPINDLE_DIRECTION_BIT);
   spindle_stop();
 }
 
+int spindle_on = 0, spindle_where = 0, spindle_up, spindle_mult = 2, spindle_initial;
 void spindle_stop()
 {
-  SPINDLE_ENABLE_PORT &= ~(1<<SPINDLE_ENABLE_BIT);
+  spindle_up = -1;
 }
 
-void spindle_run(int8_t direction) //, uint16_t rpm) 
+
+ISR(TIMER1_OVF_vect)
 {
-  if (direction != current_direction) {
-    plan_synchronize();
-    if (direction) {
-      if(direction > 0) {
-        SPINDLE_DIRECTION_PORT &= ~(1<<SPINDLE_DIRECTION_BIT);
-      } else {
-        SPINDLE_DIRECTION_PORT |= 1<<SPINDLE_DIRECTION_BIT;
+
+
+   if (!spindle_on) {
+    SPINDLE_ENABLE_PORT |= 1<<SPINDLE_ENABLE_BIT;
+    spindle_on = 1;
+
+    if (spindle_up > 0) {
+      if (spindle_where < 32000) {
+        if (spindle_initial > 128) {
+          spindle_where+=spindle_mult;
+          spindle_mult++;
+        } else {
+          spindle_initial+=8;
+          spindle_where+=1;
+        }
       }
-      SPINDLE_ENABLE_PORT |= 1<<SPINDLE_ENABLE_BIT;
-    } else {
-      spindle_stop();     
+    } else if (spindle_up < 0) {
+      spindle_where-=32;
     }
-    current_direction = direction;
-  }
+
+    TCNT1  = (MAX) - spindle_where; // Reload timer with precalculated value
+   } else {
+     SPINDLE_ENABLE_PORT &= ~(1<<SPINDLE_ENABLE_BIT);
+     spindle_on = 0;
+     TCNT1  = MAX-2960;
+   }
+
+   if (spindle_up == -1 && spindle_where < 32) {
+    TCCR1B = 0;
+   }
+
+}
+
+void spindle_run(int8_t direction) //, uint16_t rpm)
+{
+  spindle_up = 1;
+  spindle_initial = 0;
+  spindle_where=16650;
+  spindle_mult=1;
+  TIMSK1 |= (1 << TOIE1); // Enable overflow interrupt
+  TCNT1 = 0; // Preload timer with precalculated value
+  TCCR1B |= (1 << CS10);
 }
